d58044b70968d13b62879403305eb3cc002a7c22
Alex Vanderhoeff <74937141+avand56@users.noreply.github.com>
2022-03-17 10:52:35 -0600

    Update hms_string.py

--- hms_string.py
+++ hms_string.py
@@ -3,4 +3,4 @@ def hms_string(sec_elapsed):
     h = int(sec_elapsed / (60 * 60))
     m = int((sec_elapsed % (60 * 60)) / 60)
     s = sec_elapsed % 60
-    return "{}:{:>02}:{:>05.2f}".format(h, m, s)
+    return "{}:{:>02}:{:>05.2f}".format(h, m, s) #
